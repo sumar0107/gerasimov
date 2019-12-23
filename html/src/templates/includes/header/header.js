5da73ee32f6642d00126a666e8c8610bcef13968
antonsumarokov <anton.sumarokov@deasign.ru>
2019-12-23 19:06:02 +0300

    правки стили

--- html/src/templates/includes/header/header.js
+++ html/src/templates/includes/header/header.js
@@ -1,7 +1,11 @@
 const headerFixTop = () => {
-  const sticky = 7
-  // const header = document.querySelector('header.header')
+  const sticky = 7;
+  const header = document.querySelector('header.header')
   const headerLid = document.querySelector('.header-lid')
+  let didScroll;
+  let lastScrollTop = 0;
+  const delta = 5;
+  const headerHeight = header.clientHeight;
 
   function elFixTop(el) {
     if (window.pageYOffset > sticky) {
@@ -19,5 +23,45 @@ const headerFixTop = () => {
       elFixTop(headerLid)
     })
   }
+
+  function hasScrolled() {
+    const st = $(window).scrollTop();
+    // Make scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
+
+    // If scrolled down and past the navbar, add class .nav-up.
+    if (st > lastScrollTop && st > headerHeight) {
+      // Scroll Down
+      document.body.classList.remove('head-effect-down');
+      document.body.classList.add('head-effect-up');
+    } else if (st + $(window).height() < $(document).height()) {
+      // Scroll Up
+      document.body.classList.remove('head-effect-up');
+      document.body.classList.add('head-effect-down');
+    }
+
+    lastScrollTop = st;
+  }
+
+  window.addEventListener('scroll', (event) => {
+    didScroll = true
+  });
+  setInterval(function () {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
 };
 export default headerFixTop
+// Hide header on scroll down
+
+
+
+
+
+
+
+
