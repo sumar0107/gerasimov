a8f698327de6e2dac2634a18e7984c019c9ef8a6
antonsumarokov <anton.sumarokov@deasign.ru>
2019-12-27 16:23:51 +0300

    js webpack fix

--- html/webpack.config.js
+++ html/webpack.config.js
@@ -55,7 +55,29 @@ function createConfig(env) {
       }),
     ],
     optimization :{
-      minimize: isProduction
+      minimize: isProduction,
+      splitChunks: {
+        chunks: 'async',
+        minSize: 30000,
+        // minRemainingSize: 0,
+        maxSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 6,
+        maxInitialRequests: 4,
+        automaticNameDelimiter: '~',
+        automaticNameMaxLength: 30,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      }
     },
     module: {
       rules: [
