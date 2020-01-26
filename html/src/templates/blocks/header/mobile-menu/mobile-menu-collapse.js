a877170201e1121f42698a59891273c04cb5df0c
antonsumarokov <anton.sumarokov@deasign.ru>
2020-01-27 00:54:46 +0300

    правки

--- html/src/templates/blocks/header/mobile-menu/mobile-menu-collapse.js
+++ html/src/templates/blocks/header/mobile-menu/mobile-menu-collapse.js
@@ -1,9 +1,16 @@
 const menu = () => {
   if ($('.mobile-menu__substrate').length > 0) {
     const $menuSubstrate = $('.mobile-menu__substrate')
+    const $header = $('.header')
     $('#menu')
-      .on('show.bs.collapse', () => $menuSubstrate.collapse('toggle'))
-      .on('hide.bs.collapse', () => $menuSubstrate.collapse('toggle'))
+      .on('show.bs.collapse', () => {
+        $header.addClass('z-i-13')
+        $menuSubstrate.collapse('toggle')
+      })
+      .on('hide.bs.collapse', () => {
+        $header.removeClass('z-i-13')
+        $menuSubstrate.collapse('toggle')
+      })
   }
 
 };
